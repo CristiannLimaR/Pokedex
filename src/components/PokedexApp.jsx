fd5652e9ad6d1deef7f85ff3924b43c371d6519a
Cristian Lima <cristiannlima2@gmail.com>
2024-08-30 23:16:58 -0600

    Finalizacion de pokedex

--- src/components/PokedexApp.jsx
+++ src/components/PokedexApp.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import {BuscarPokemon} from '../components/BuscarPokemon'
-import { usePokemon } from '../hooks/usePokemon'
-import { ContenedorPokemon } from './ContenedorPokemon'
+import React from "react";
+import { BuscarPokemon } from "../components/BuscarPokemon";
+import { usePokemon } from "../hooks/usePokemon";
+import { ContenedorPokemon } from "./ContenedorPokemon";
 
 export const PokedexApp = () => {
-   const {handleGetPokemon,datosPokemon} = usePokemon()
+  const { handleGetPokemon, datosPokemon } = usePokemon();
+
   return (
     <>
-    <BuscarPokemon handleGetPokemon= {handleGetPokemon}/>
-    <ContenedorPokemon/>
+      <BuscarPokemon handleGetPokemon={handleGetPokemon} />
+      <ContenedorPokemon datosPokemon={datosPokemon}/>
     </>
-    
-  )
-}
+  );
+};
